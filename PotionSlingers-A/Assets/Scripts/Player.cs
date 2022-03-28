0b2b3e70906bc1187e7f7823141d4d10a876b3e7
Denzill Loe <dloe@mail.sfsu.edu>
2022-03-27 23:29:11 -0700

    Added several scripts, small refactoring of artifact and vessel

---
+++ PotionSlingers-A/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int hp;
+    public Deck deck;
+    public Holster holster;
+
+    public Player()
+    {
+
+    }
+
+    public Player(int Hp)
+    {
+        hp = Hp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
