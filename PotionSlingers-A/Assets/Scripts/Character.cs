6fb6fffff8236fd2b62562d22fbc5d0bb7c2d316
Jason Fujii <Jasonfujii@yahoo.com>
2022-04-01 23:03:19 -0700

    Player class update and Character creation

---
+++ PotionSlingers-A/Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class Character : ScriptableObject, IPointerDownHandler
+{
+    public int id;
+    public string cardName;
+    public string desc;
+    public Sprite image;
+    public string flipTask;
+    public bool flipped;
+    public Sprite flippedImage;
+    public string flippedDesc;
+    
+    public void OnPointerDown(PointerEventData pointerEventData) {
+        flipped = !flipped;
+        Debug.Log(cardName + " has been clicked. Flipped is now: " + flipped);
+    }
+}
