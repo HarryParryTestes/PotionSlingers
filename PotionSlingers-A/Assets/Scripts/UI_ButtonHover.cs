3440dac84445b8189b550dbe28e5d550241738c5
Matteo <61305877+HarryParryTestes@users.noreply.github.com>
2022-04-07 00:19:56 -0700

    add hover stuff

--- PotionSlingers-A/Assets/Scripts/UI_ButtonHover.cs
+++ PotionSlingers-A/Assets/Scripts/UI_ButtonHover.cs
@@ -5,16 +5,16 @@ using UnityEngine.EventSystems;
 
 public class UI_ButtonHover : MonoBehaviour, IPointerEnterHandler
 {
-    Vector3 cachedScale; // Tracks original size of Card (for hovering as it manipulates scale of Card).
-    Vector3 originalPos; // Tracks original position of Card.
+    Vector3 cachedScale; // Tracks original size of button
+    Vector3 originalPos; // Tracks original position of button.
     Vector3 mousePos; // Tracks current mouse cursor position.
 
     RectTransform rt;
 
-    // canHover is public static because if not static, other cards can be hovered
-    // over while a card is clicked and attached to cursor.
-    public static bool canHover = true; // Determines if cards can be hovered over.
-    bool attached = false; // Determines if a card has been clicked and attached to the mouse cursor.
+    // canHover is public static because if not static, other button can be hovered
+    // over while a button is clicked and attached to cursor.
+    public static bool canHover = true; // Determines if button can be hovered over.
+   
 
     // On startup:
     void Start()
@@ -32,10 +32,10 @@ public class UI_ButtonHover : MonoBehaviour, IPointerEnterHandler
         if (canHover)
         {
             
-            // transform.position = new Vector3(transform.position.x, height + height/2, transform.position.z);
+          
 
-            // Card Hover sound effect:
-            FMODUnity.RuntimeManager.PlayOneShot("event:/UI/UI_Card_Hover");
+            // Button Hover sound effect:
+            FMODUnity.RuntimeManager.PlayOneShot("event:/UI/UI_Menu_Hover");
         }
     }
     
