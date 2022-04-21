6426c7db88f4f0808970ad35035787894240f6d9
faisal-zaheervi <faisalszaheer@gmail.com>
2022-04-20 21:54:27 -0700

    Progress on Card Menus; Can view card with View button, but needs fixes.

--- PotionSlingers-A/Assets/Scripts/Hover_Card.cs
+++ PotionSlingers-A/Assets/Scripts/Hover_Card.cs
@@ -16,11 +16,28 @@ public class Hover_Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public static bool canHover = true; // Determines if cards can be hovered over.
     bool attached = false; // Determines if a card has been clicked and attached to the mouse cursor.
 
+    Transform cardMenu;
+    Transform viewCardMenu;
+    bool cardSelected = false;
+    public static bool viewingCard = false;
+
     // On startup:
     void Start() {
         rt = transform.GetComponent<RectTransform>();
         cachedScale = transform.localScale;
-        originalPos = gameObject.transform.position;    
+        originalPos = gameObject.transform.position;
+        cardMenu = this.transform.Find("CardMenu");
+        viewCardMenu = this.transform.Find("ViewCardMenu");
+        if(cardMenu == null) {
+            Debug.Log("Error: CardMenu doesn't exist!");
+        }
+        if(viewCardMenu == null) {
+            Debug.Log("Error: ViewCardMenu doesn't exist!");
+        }
+        cardSelected = false;
+        viewingCard = false;
+        cardMenu.gameObject.SetActive(false);
+        viewCardMenu.gameObject.SetActive(false);
     }
 
     void Update() {
@@ -37,7 +54,8 @@ public class Hover_Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         if(canHover) {
             float width = rt.sizeDelta.x * rt.localScale.x;
             float height = rt.sizeDelta.y * rt.localScale.y;
-            transform.SetSiblingIndex(transform.childCount - 1); // Sets card 
+            transform.SetSiblingIndex(this.transform.parent.transform.childCount - 1);
+            //transform.SetSiblingIndex(transform.childCount - 1); // Sets card 
             transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
             // transform.position = new Vector3(transform.position.x, height + height/2, transform.position.z);
 
@@ -52,16 +70,31 @@ public class Hover_Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     {
         // If Card is clicked on and attached to cursor, other cards aren't hoverable,
         // else, if no Card is not attached anymore, Cards are hoverable again.
-        attached = !attached;
-        canHover = !canHover;
-        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/UI_Card_Pickup");
-        if(attached) {
-            mousePos = Input.mousePosition;
-            this.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
-        }
-        if(!attached && canHover) {
-            transform.localScale = cachedScale;
-            originalPos = gameObject.transform.position;
+
+        // attached = !attached;
+        // canHover = !canHover;
+        // FMODUnity.RuntimeManager.PlayOneShot("event:/UI/UI_Card_Pickup");
+        // if(attached) {
+        //     mousePos = Input.mousePosition;
+        //     this.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        // }
+        // if(!attached && canHover) {
+        //     transform.localScale = cachedScale;
+        //     originalPos = gameObject.transform.position;
+        // }
+        if(!viewingCard) {
+            cardSelected = !cardSelected;
+            if(cardSelected) {
+                canHover = false;
+                transform.localScale = cachedScale;
+                gameObject.transform.position = originalPos;
+                cardMenu.gameObject.SetActive(true);
+            }
+            else if (!cardSelected) {
+                transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+                canHover = true;
+                cardMenu.gameObject.SetActive(false);
+            }
         }
     }
  
@@ -71,6 +104,28 @@ public class Hover_Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         if(canHover) {
             transform.localScale = cachedScale;
             gameObject.transform.position = originalPos;
+        }
+    }
+
+    public void ViewCard() {
+        viewingCard = true;
+        if(cardSelected) {
+            cardMenu.gameObject.SetActive(false);
+            transform.SetSiblingIndex(this.transform.parent.transform.childCount - 1);
+            transform.localScale = new Vector3(4f, 4f, 4f);
+            this.transform.position = new Vector3(255, 285, 0);
+            viewCardMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void resetView() {
+        viewingCard = false;
+        viewCardMenu.gameObject.SetActive(false);
+        if(cardSelected) {
+            cardSelected = false;
+            transform.localScale = cachedScale;
+            gameObject.transform.position = originalPos;
+            canHover = true;
         }
     }
 }
