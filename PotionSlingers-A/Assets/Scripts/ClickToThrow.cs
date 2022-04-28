443fd9fefdf060956fb4624e185bcec8004c66af
Denzill Loe <dloe@mail.sfsu.edu>
2022-04-27 23:10:31 -0700

    Fixed some stuff in GameScene to get GameEvents configured

---
+++ PotionSlingers-A/Assets/Scripts/ClickToThrow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToThrow : MonoBehaviour
+{
+    [SerializeField] GameEvent ThrowPotion;
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        Throw();
+    }
+
+    // Update is called once per frame
+    void Throw()
+    {
+        ThrowPotion?.Invoke();
+    }
+}
